c1786f0b5ed51f2f9d73be20de3acc43bf1ae566
Lenilson Inhari <lenilsonip@gmail.com>
2025-04-13 21:58:46 -0300

    Ajustes

--- src/main/java/com/br/luc/ci/cd/teste_cicd/controller/ProdutoController.java
+++ src/main/java/com/br/luc/ci/cd/teste_cicd/controller/ProdutoController.java
@@ -1,14 +1,21 @@
 package com.br.luc.ci.cd.teste_cicd.controller;
 
-import com.br.luc.ci.cd.teste_cicd.model.Produto;
-import com.br.luc.ci.cd.teste_cicd.service.ProdutoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.luc.ci.cd.teste_cicd.model.Produto;
+import com.br.luc.ci.cd.teste_cicd.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produtos")
